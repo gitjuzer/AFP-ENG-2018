433ff05236fea002fcbcc37f3a7ab3107434fc93
Amer Mamoun <amer.mn.96@hotmail.com>
2018-12-30 20:42:39 +0100

    pics link

--- client/PickIT/PickIT/Forms/ExerciseEngine.cs
+++ client/PickIT/PickIT/Forms/ExerciseEngine.cs
@@ -12,10 +12,20 @@ namespace PickIT
 {
     public partial class ExerciseEngine : Form
     {
-        public ExerciseEngine(string Exercise)
+        static Random rnd = new Random();
+        public ExerciseEngine(/*string Exercise*/)
         {
             InitializeComponent();
+            int folder;
+            folder = rnd.Next(0, 4);
 
+            p1.ImageLocation = "imgfolder/ex1/correct.jpg";
+
+            p1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void cmd_submite_Click(object sender, EventArgs e)
+        {
 
         }
     }
--- client/PickIT/PickIT/Program.cs
+++ client/PickIT/PickIT/Program.cs
@@ -16,7 +16,7 @@ namespace PickIT
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new ExerciseEngine());
         }
     }
 }
