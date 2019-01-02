19d55428763780428ce1a6ee2172af926ebaf53c
Amer Mamoun <amer.mn.96@hotmail.com>
2019-01-02 15:52:52 +0100

    password change

--- client/PickIT/PickIT/Forms/Login.Designer.cs
+++ client/PickIT/PickIT/Forms/Login.Designer.cs
@@ -38,62 +38,60 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(194, 137);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(259, 169);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(77, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(197, 183);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(263, 225);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(73, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password:";
             // 
             // username_txt
             // 
-            this.username_txt.Location = new System.Drawing.Point(263, 135);
-            this.username_txt.Margin = new System.Windows.Forms.Padding(2);
+            this.username_txt.Location = new System.Drawing.Point(351, 166);
+            this.username_txt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.username_txt.Name = "username_txt";
-            this.username_txt.Size = new System.Drawing.Size(76, 20);
+            this.username_txt.Size = new System.Drawing.Size(100, 22);
             this.username_txt.TabIndex = 2;
             // 
             // password_txt
             // 
-            this.password_txt.Location = new System.Drawing.Point(263, 183);
-            this.password_txt.Margin = new System.Windows.Forms.Padding(2);
+            this.password_txt.Location = new System.Drawing.Point(351, 225);
+            this.password_txt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.password_txt.Name = "password_txt";
-            this.password_txt.Size = new System.Drawing.Size(76, 20);
+            this.password_txt.PasswordChar = '*';
+            this.password_txt.Size = new System.Drawing.Size(100, 22);
             this.password_txt.TabIndex = 3;
             // 
             // cmd_login
             // 
-            this.cmd_login.Location = new System.Drawing.Point(254, 226);
-            this.cmd_login.Margin = new System.Windows.Forms.Padding(2);
+            this.cmd_login.Location = new System.Drawing.Point(339, 278);
+            this.cmd_login.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmd_login.Name = "cmd_login";
-            this.cmd_login.Size = new System.Drawing.Size(85, 38);
+            this.cmd_login.Size = new System.Drawing.Size(113, 47);
             this.cmd_login.TabIndex = 4;
             this.cmd_login.Text = "Login and Get Exercises";
             this.cmd_login.UseVisualStyleBackColor = true;
             this.cmd_login.Click += new System.EventHandler(this.cmd_login_Click);
-
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.cmd_login);
             this.Controls.Add(this.password_txt);
             this.Controls.Add(this.username_txt);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
